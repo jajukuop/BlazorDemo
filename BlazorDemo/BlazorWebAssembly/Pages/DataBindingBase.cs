59e3950474050e1dccbf554c235f6d1a48b15440
kuoppa <jajukuop@gmail.com>
2020-11-23 17:34:36 +0200

    Finished second demo

---
+++ BlazorDemo/BlazorWebAssembly/Pages/DataBindingBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWebAssembly.Pages
+{
+    public class DataBindingBase : ComponentBase
+    {
+        [Parameter]
+        public RenderFragment HeadingHtml { get; set; }
+
+        [Parameter]
+        public string FirstName { get; set; } = "Teppo";
+        [Parameter]
+        public string LastName { get; set; } = "Demo";
+    }
+}
